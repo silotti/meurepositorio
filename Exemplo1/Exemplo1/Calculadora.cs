24e525f7577c9b6539198fe7054b2381167ac295
Leonardo Luiz Silotti <silotti@gmail.com>
2018-06-16 19:41:32 -0300

    Criei um projeto de exmplo

---
+++ Exemplo1/Exemplo1/Calculadora.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo1
+{
+    class Calculadora
+    {
+        static void Main(string[] args)
+        {
+        }
+        public int Soma (int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
